2413847004cd11004c63715e7c73f7f2c1cd46ec
Muwaffaq Elbadawi <kingmoaffag10@gmail.com>
2023-09-09 20:19:38 +0200

    designing the field

--- js/main.js
+++ js/main.js
@@ -1,1 +1,27 @@
-const 
+const firstName = document.getElementById("firsName");
+const lastName = document.getElementById("lasName");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+
+
+
+const userDB = [];
+
+const user = {
+    firsName: "Muwaffaq",
+    lasName: "Elbadawi",
+    email: "muwaffaq@elbadawi.com",
+    password: 123
+};
+
+
+const addNewUser = (user) => {
+    userDB.push(user);
+    return userDB;
+};
+
+// console.log(addNewUser(user));
+
+console.log(firstName.value);
+console.log(lastName.value);
+
