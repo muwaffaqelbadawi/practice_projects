490ed26b056bdf4df6dd7388a71d6a6b7fbed4a2
Muwaffaq Elbadawi <kingmoaffag10@gmail.com>
2023-09-11 08:20:02 +0200

    Morning ganged

--- js/main.js
+++ js/main.js
@@ -1,27 +1,83 @@
-const firstName = document.getElementById("firsName");
-const lastName = document.getElementById("lasName");
-const email = document.getElementById("email");
-const password = document.getElementById("password");
+let usersDB = [];
 
+document.getElementById("add").addEventListener("click", (event) => {
+  event.preventDefault();
+  const firstName = document.getElementById("firstName");
+  const lastName = document.getElementById("lastName");
+  const email = document.getElementById("email");
+  const password = document.getElementById("password");
+  const user = [firstName.value, lastName.value, email.value, password.value];
 
+  addNewUser(user);
+  clearInput();
+});
 
-const userDB = [];
+document.getElementById("delete").addEventListener("click", (event) => {
+  event.preventDefault();
+  const userInput = document.getElementById("id");
+  const id = Number(userInput.value);
 
-const user = {
-    firsName: "Muwaffaq",
-    lasName: "Elbadawi",
-    email: "muwaffaq@elbadawi.com",
-    password: 123
-};
-
+  deleteUser(id);
+  clearInput();
+});
 
 const addNewUser = (user) => {
-    userDB.push(user);
-    return userDB;
+  const [firstName, lastName, email, password] = user;
+  const id = usersDB.length === 0 ? 1 : usersDB[usersDB.length - 1].id + 1;
+
+  usersDB.push({
+    id: id,
+    firstName: firstName,
+    lastName: lastName,
+    email: email,
+    password: password,
+  });
+
+  display(usersDB);
+  return usersDB;
 };
 
-// console.log(addNewUser(user));
+const deleteUser = (id) => {
+  const newUsersDB = usersDB.filter((user) => user.id !== id);
+  usersDB = newUsersDB;
 
-console.log(firstName.value);
-console.log(lastName.value);
+  display(usersDB);
+  //   return usersDB;
+};
 
+const clearInput = () => {
+  id.value = "";
+  firstName.value = "";
+  lastName.value = "";
+  email.value = "";
+  password.value = "";
+};
+
+const list = (users) => {
+  let userItem = "";
+  users.forEach((user) => {
+    userItem +=
+      "<li>Name: " +
+      user.firstName +
+      " " +
+      user.lastName +
+      " ID: " +
+      user.id +
+      "</li>";
+    document.getElementById("item").innerHTML = userItem;
+  });
+};
+
+const count = (users) => {
+  document.getElementById(
+    "item-count"
+  ).innerHTML = `You have ${users.length} items in the list`;
+};
+
+const display = (usersDB) => {
+  /* if (!usersDB.length) {
+  } */
+
+  list(usersDB);
+  count(usersDB);
+};
