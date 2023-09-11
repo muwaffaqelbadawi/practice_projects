49da59c2c922dc4bc82c56211540bb33bb21bd0f
Muwaffaq Elbadawi <kingmoaffag10@gmail.com>
2023-09-11 18:25:02 +0200

    put and remove

--- js/main.js
+++ js/main.js
@@ -1,83 +1,110 @@
 let usersDB = [];
 
-document.getElementById("add").addEventListener("click", (event) => {
+document.getElementById("put").addEventListener("click", (event) => {
   event.preventDefault();
-  const firstName = document.getElementById("firstName");
-  const lastName = document.getElementById("lastName");
-  const email = document.getElementById("email");
-  const password = document.getElementById("password");
-  const user = [firstName.value, lastName.value, email.value, password.value];
-
-  addNewUser(user);
-  clearInput();
+  const params = getUserInput();
+  const user = createUser(params);
+  put(user);
 });
 
-document.getElementById("delete").addEventListener("click", (event) => {
+document.getElementById("update").addEventListener("click", (event) => {
   event.preventDefault();
-  const userInput = document.getElementById("id");
-  const id = Number(userInput.value);
-
-  deleteUser(id);
-  clearInput();
+  /* const params = getUserInput();
+  const user = createUser(params);
+  update(user); */
 });
 
-const addNewUser = (user) => {
-  const [firstName, lastName, email, password] = user;
+document.getElementById("remove").addEventListener("click", (event) => {
+  event.preventDefault();
+  const id = getRemoveId();
+  remove(id);
+});
+
+const put = (user) => {
   const id = usersDB.length === 0 ? 1 : usersDB[usersDB.length - 1].id + 1;
-
-  usersDB.push({
-    id: id,
-    firstName: firstName,
-    lastName: lastName,
-    email: email,
-    password: password,
-  });
+  user.id = id;
 
   display(usersDB);
   return usersDB;
 };
 
-const deleteUser = (id) => {
-  const newUsersDB = usersDB.filter((user) => user.id !== id);
-  usersDB = newUsersDB;
+const update = (userObj) => {
+  // const index = usersDB.findIndex((user) => user.id === userObj.id);
 
   display(usersDB);
-  //   return usersDB;
+  return usersDB;
 };
 
-const clearInput = () => {
-  id.value = "";
-  firstName.value = "";
-  lastName.value = "";
-  email.value = "";
-  password.value = "";
+const remove = (id) => {
+  const idExist = usersDB.some((user) => user.id === id);
+  !idExist
+    ? console.log(`Id ${id} does not exist`)
+    : (usersDB = usersDB.filter((user) => user.id !== id));
+
+  display(usersDB);
+  return usersDB;
 };
 
 const list = (users) => {
   let userItem = "";
-  users.forEach((user) => {
-    userItem +=
-      "<li>Name: " +
-      user.firstName +
-      " " +
-      user.lastName +
-      " ID: " +
-      user.id +
-      "</li>";
-    document.getElementById("item").innerHTML = userItem;
-  });
+  !users.length
+    ? (userItem = "")
+    : users.forEach((user) => {
+        userItem += `<li>Name: ${user.firstName} ${user.lastName} ID: ${user.id}</li>`;
+      });
+  document.getElementById("item").innerHTML = userItem;
 };
 
 const count = (users) => {
-  document.getElementById(
-    "item-count"
-  ).innerHTML = `You have ${users.length} items in the list`;
+  document.getElementById("user-count").innerText = `${users.length} ${
+    users.length <= 1 ? "user" : "users"
+  } registered`;
 };
 
-const display = (usersDB) => {
-  /* if (!usersDB.length) {
-  } */
+const createUser = (params) => {
+  const [firstName, lastName, email, password] = params;
+  const userObj = {
+    firstName: firstName,
+    lastName: lastName,
+    email: email,
+    password: password,
+  };
+  return userObj;
+};
 
-  list(usersDB);
-  count(usersDB);
+const getUserInput = () => {
+  const firstNameField = document.getElementById("firstName");
+  const lastNameField = document.getElementById("lastName");
+  const emailField = document.getElementById("email");
+  const passwordField = document.getElementById("password");
+  const userParams = [
+    firstName.value,
+    lastName.value,
+    email.value,
+    password.value,
+  ];
+
+  clear([firstNameField, lastNameField, emailField, passwordField]);
+  return userParams;
+};
+
+const getRemoveId = () => {
+  const idField = document.getElementById("remove-id");
+  const id = Number(idField.value);
+
+  clear(idField);
+  return id;
+};
+
+const display = (users) => {
+  list(users);
+  count(users);
+};
+
+const clear = (field) => {
+  Array.isArray(field)
+    ? field.forEach((input) => {
+        input.value = "";
+      })
+    : (field.value = "");
 };
